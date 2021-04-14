4679c7843d8364c493a02c35271d53a1388e7ac4
Tavo <ap3703213@gmail.com>
2021-04-13 23:10:51 -0500

    Primera Version de Login con Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA_-RZ0vt5QEihLMGasxJukpcCBdn-ku9w",
+    authDomain: "applogin-6fa3c.firebaseapp.com",
+    projectId: "applogin-6fa3c",
+    storageBucket: "applogin-6fa3c.appspot.com",
+    messagingSenderId: "898380650909",
+    appId: "1:898380650909:web:fbd38e9edc64383853b774"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
